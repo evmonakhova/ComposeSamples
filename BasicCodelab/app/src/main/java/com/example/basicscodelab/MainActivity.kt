1fe9ad3e40ddd299dda40c72de7023c73f1d984f
Elena Monakhova <e.monakhova@okko.tv>
2021-06-21 16:15:12 +0300

    State in compose

--- BasicCodelab/app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ BasicCodelab/app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -3,10 +3,16 @@ package com.example.basicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Button
+import androidx.compose.material.Divider
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -18,7 +24,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
-                Greeting("Android")
+                MyScreenContent()
             }
         }
     }
@@ -34,9 +40,39 @@ fun MyApp(content: @Composable () -> Unit) {
 }
 
 @Composable
+fun MyScreenContent(names: List<String> = listOf("Android", "there", "Alyona")) {
+    val counterState = remember { mutableStateOf(0) }
+
+    Column(
+        modifier = Modifier.width(150.dp)
+    ) {
+        for (name in names) {
+            Greeting(name = name)
+            Divider(color = Color.Black)
+        }
+        Counter(
+            count = counterState.value,
+            updateCount = { newCount ->
+                counterState.value = newCount
+            }
+        )
+    }
+}
+
+@Composable
 fun Greeting(name: String) {
     Surface(color = Color.Yellow) {
         Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    }
+}
+
+@Composable
+fun Counter(count: Int, updateCount: (Int) -> Unit) {
+    Button(
+        modifier = Modifier.padding(24.dp),
+        onClick = { updateCount(count + 1) }
+    ) {
+        Text("I've been clicked $count times")
     }
 }
 
@@ -44,6 +80,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     MyApp {
-        Greeting("Android")
+        MyScreenContent()
     }
 }
