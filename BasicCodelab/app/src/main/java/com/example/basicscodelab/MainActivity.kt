7638ed2678612058616d45aec53627f0847de01f
Elena Monakhova <e.monakhova@okko.tv>
2022-03-17 19:58:34 +0300

    Replace button with icon, use string res, animateContentSize instead of manual animations, add elevation and shapes

--- BasicCodelab/app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ BasicCodelab/app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -4,21 +4,21 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,30 +28,26 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApp {
-                var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
-
-                if (shouldShowOnboarding) {
-                    OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
-                } else {
-                    MyScreenContent()
-                }
+            BasicsCodelabAppTheme {
+                MyApp()
             }
         }
     }
 }
 
 @Composable
-fun MyApp(content: @Composable () -> Unit) {
-    BasicsCodelabAppTheme {
-        Surface(color = MaterialTheme.colors.surface) {
-            content()
-        }
+private fun MyApp() {
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+
+    if (shouldShowOnboarding) {
+        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+    } else {
+        Greetings()
     }
 }
 
 @Composable
-fun OnboardingScreen(onContinueClicked: () -> Unit) {
+private fun OnboardingScreen(onContinueClicked: () -> Unit) {
     Surface {
         Column(
             modifier = Modifier.fillMaxSize(),
@@ -60,8 +56,7 @@ fun OnboardingScreen(onContinueClicked: () -> Unit) {
         ) {
             Text("Welcome to the Basics Codelab!")
             Button(
-                modifier = Modifier
-                    .padding(vertical = 24.dp),
+                modifier = Modifier.padding(vertical = 24.dp),
                 onClick = onContinueClicked
             ) {
                 Text("Continue")
@@ -71,103 +66,68 @@ fun OnboardingScreen(onContinueClicked: () -> Unit) {
 }
 
 @Composable
-fun MyScreenContent(names: List<String> = listOf("Android", "there", "Alyona")) {
-    val counterState = remember { mutableStateOf(0) }
-
-    Column(
-        modifier = Modifier.fillMaxHeight()
-    ) {
-        NameList(names = List(1000) { "#$it" })
-        Counter(
-            count = counterState.value,
-            updateCount = { newCount ->
-                counterState.value = newCount
-            }
-        )
-    }
-}
-
-@Composable
-fun NameList(names: List<String>, modifier: Modifier = Modifier) {
-    LazyColumn(modifier = modifier) {
+private fun Greetings(names: List<String> = List(1000) { "$it" } ) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
         items(items = names) { name ->
-            GreetingWithButtons(name = name)
-            Divider(color = Color.Black)
+            Greeting(name = name)
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    var isSelected by remember { mutableStateOf(false) }
-    val backgroundColor by animateColorAsState(if (isSelected) Color.Red else Color.Transparent)
-
-    Text(
-        text = "Hello $name!",
-        modifier = Modifier
-            .padding(24.dp)
-            .background(color = backgroundColor)
-            .clickable(onClick = { isSelected = !isSelected }),
-        style = MaterialTheme.typography.body1
-    )
-}
-
-@Composable
-private fun GreetingWithButtons(name: String) {
-    var expanded by rememberSaveable { mutableStateOf(false) }
-
-    val extraPadding by animateDpAsState(
-        if (expanded) 48.dp else 0.dp,
-        animationSpec = spring(
-            dampingRatio = Spring.DampingRatioMediumBouncy,
-            stiffness = Spring.StiffnessLow
-        )
-    )
-
-    Surface(
-        color = MaterialTheme.colors.primary,
+private fun Greeting(name: String) {
+    Card(
+        backgroundColor = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier
+        CardContent(name)
+    }
+}
+
+@Composable
+private fun CardContent(name: String) {
+    var expanded by rememberSaveable { mutableStateOf(false) }
+
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+    ) {
+        Column(
+            modifier = Modifier
                 .weight(1f)
-                .padding(bottom = extraPadding.coerceAtLeast(0.dp))
-            ) {
-                Text(text = "Hello, ")
+                .padding(12.dp)
+        ) {
+            Text(text = "Hello, ")
+            Text(
+                text = name,
+                style = MaterialTheme.typography.h4.copy(
+                    fontWeight = FontWeight.ExtraBold
+                )
+            )
+            if (expanded) {
                 Text(
-                    text = name,
-                    style = MaterialTheme.typography.h4.copy(
-                        fontWeight = FontWeight.ExtraBold
-                    )
+                    text = ("Composem ipsum color sit lazy, " +
+                            "padding theme elit, sed do bouncy. ").repeat(4),
                 )
             }
-            OutlinedButton(
-                onClick = { expanded = !expanded }
-            ) {
-                Text(if (expanded) "Show less" else "Show more")
-            }
         }
-    }
-}
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(
+                imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                contentDescription = if (expanded) {
+                    stringResource(R.string.show_less)
+                } else {
+                    stringResource(R.string.show_more)
+                }
 
-@Composable
-fun Counter(count: Int, updateCount: (Int) -> Unit) {
-    Button(
-        modifier = Modifier.padding(24.dp),
-        onClick = { updateCount(count + 1) },
-        colors = ButtonDefaults.buttonColors(
-            backgroundColor = if (count > 5) Color.Green else Color.White
-        )
-    ) {
-        Text("I've been clicked $count times")
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MyApp {
-        MyScreenContent()
+            )
+        }
     }
 }
 
@@ -177,10 +137,18 @@ fun DefaultPreview() {
     uiMode = UI_MODE_NIGHT_YES,
     name = "DefaultPreviewDark"
 )
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun DefaultPreview() {
+    BasicsCodelabAppTheme {
+        Greetings()
+    }
+}
+
 @Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
 fun OnboardingPreview() {
-    MyApp {
+    BasicsCodelabAppTheme {
         OnboardingScreen(onContinueClicked = {})
     }
 }
