c796017e3f0745b0a7eb09d00e4a7f1129259dad
evmonakhova <evmonakhova@gmail.com>
2024-01-17 01:00:19 +0300

    Save state with rememberSaveable

--- BasicStateCodelab/app/src/main/java/emonakhova/codelabs/basicstate/WellnessScreen.kt
+++ BasicStateCodelab/app/src/main/java/emonakhova/codelabs/basicstate/WellnessScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -23,8 +24,8 @@ fun WellnessScreen(modifier: Modifier = Modifier) {
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count by remember { mutableStateOf(0) }
-        var showTask by remember { mutableStateOf(true) }
+        var count by rememberSaveable { mutableStateOf(0) }
+        var showTask by rememberSaveable { mutableStateOf(true) }
         if (count > 0) {
             if (showTask) {
                 WellnessTaskItem(
