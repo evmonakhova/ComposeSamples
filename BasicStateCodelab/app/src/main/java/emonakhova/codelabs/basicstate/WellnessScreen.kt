bb22f7ad7d3270c194a7ea7fdf4d94ba504d5e0c
evmonakhova <evmonakhova@gmail.com>
2024-01-19 00:37:53 +0300

    Hoist count and showTask states

--- BasicStateCodelab/app/src/main/java/emonakhova/codelabs/basicstate/WellnessScreen.kt
+++ BasicStateCodelab/app/src/main/java/emonakhova/codelabs/basicstate/WellnessScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
@@ -18,18 +17,40 @@ import emonakhova.codelabs.basicstate.ui.theme.BasicStateCodelabTheme
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
-    WaterCounter(modifier)
+    StatefulCounter(modifier)
 }
 
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier) {
+private fun StatefulCounter(modifier: Modifier) {
+    var count by rememberSaveable { mutableStateOf(0) }
+    var showTask by rememberSaveable { mutableStateOf(true) }
+    StatelessCounter(
+        count = count,
+        showTask = showTask,
+        onCloseWellnessTaskClicked = { showTask = false },
+        onAddGlassOfWaterClicked = {
+            showTask = true
+            count++
+        },
+        onClearWaterCounterClicked = { count = 0 },
+        modifier = modifier
+    )
+}
+
+@Composable
+fun StatelessCounter(
+    count: Int,
+    showTask: Boolean,
+    onCloseWellnessTaskClicked: () -> Unit,
+    onAddGlassOfWaterClicked: () -> Unit,
+    onClearWaterCounterClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count by rememberSaveable { mutableStateOf(0) }
-        var showTask by rememberSaveable { mutableStateOf(true) }
         if (count > 0) {
             if (showTask) {
                 WellnessTaskItem(
-                    onClose = { showTask = false },
+                    onClose = onCloseWellnessTaskClicked,
                     taskName = "Have you taken your 15 minute walk today?"
                 )
             }
@@ -40,16 +61,13 @@ fun WaterCounter(modifier: Modifier = Modifier) {
         }
         Row(Modifier.padding(top = 8.dp)) {
             Button(
-                onClick = {
-                    showTask = true
-                    count++
-                },
+                onClick = onAddGlassOfWaterClicked,
                 enabled = count < 10
             ) {
                 Text("Add one")
             }
             Button(
-                onClick = { count = 0 },
+                onClick = onClearWaterCounterClicked,
                 modifier = Modifier.padding(start = 8.dp)
             ) {
                 Text("Clear water counter")
