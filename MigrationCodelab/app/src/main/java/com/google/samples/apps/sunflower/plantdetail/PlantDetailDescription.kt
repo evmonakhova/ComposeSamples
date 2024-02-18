d8747e60d9b6c878466bd33583e12eac291845f7
evmonakhova <evmonakhova@gmail.com>
2024-02-19 01:11:42 +0300

    Migrate plant description

--- MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -16,6 +16,8 @@
 
 package com.google.samples.apps.sunflower.plantdetail
 
+import android.text.method.LinkMovementMethod
+import android.widget.TextView
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -25,12 +27,16 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.text.HtmlCompat
 import com.google.samples.apps.sunflower.R
 import com.google.samples.apps.sunflower.data.Plant
 
@@ -50,16 +56,14 @@ fun PlantDetailContent(plant: Plant) {
         .padding(horizontal = dimensionResource(id = R.dimen.margin_small))
         .wrapContentWidth(Alignment.CenterHorizontally)
 
-    Column {
+    Column(Modifier.padding(dimensionResource(R.dimen.margin_normal))) {
         PlantName(name = plant.name, modifier = centeringModifier)
-        WateringHeader(
-            modifier = centeringModifier
-                .padding(top = dimensionResource(id = R.dimen.margin_normal))
-        )
+        WateringHeader(modifier = centeringModifier)
         PlantWatering(
             wateringInterval = plant.wateringInterval,
             modifier = centeringModifier
         )
+        PlantDescription(description = plant.description, modifier = centeringModifier)
     }
 }
 
@@ -78,7 +82,7 @@ private fun WateringHeader(modifier: Modifier) {
         text = stringResource(id = R.string.watering_needs_prefix),
         color = MaterialTheme.colors.primaryVariant,
         fontWeight = FontWeight.Bold,
-        modifier = modifier
+        modifier = modifier.padding(top = dimensionResource(id = R.dimen.margin_normal))
     )
 }
 
@@ -91,4 +95,40 @@ private fun PlantWatering(wateringInterval: Int, modifier: Modifier) {
         text = wateringIntervalText,
         modifier = modifier
     )
+}
+
+@Composable
+private fun PlantDescription(description: String, modifier: Modifier) {
+    val htmlDescription = remember(description) {
+        HtmlCompat.fromHtml(description, HtmlCompat.FROM_HTML_MODE_COMPACT)
+    }
+    AndroidView(
+        factory = { context ->
+            TextView(context).apply {
+                movementMethod = LinkMovementMethod.getInstance()
+            }
+        },
+        update = {
+            it.text = htmlDescription
+        },
+        modifier = modifier.padding(top = dimensionResource(id = R.dimen.margin_small))
+    )
+}
+
+@Preview
+@Composable
+private fun PlantDetailContentPreview() {
+    MaterialTheme {
+        val plant = Plant(
+            plantId = "id",
+            name = "Apple",
+            description = "HTML<br><br>description",
+            growZoneNumber = 3,
+            wateringInterval = 30,
+            imageUrl = ""
+        )
+        MaterialTheme {
+            PlantDetailContent(plant)
+        }
+    }
 }
